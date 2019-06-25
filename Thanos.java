fa40b7fdda08965bec49ed96f1e751cdd6fea1e3
Clément Bouakil <bouakil.clement.pro@gmail.com>
2019-06-25 13:32:33 +0200

    création des instances + ajout à la liste + modification de l'age de thor

--- Thanos.java
+++ Thanos.java
@@ -4,21 +4,28 @@ public class Thanos {
 
     public static void main(String[] args) {
 
-        // TODO 1 : Create an empty heroes list
+        Hero heroOne = new Hero("Black Widow", 34);
+        Hero heroTwo = new Hero("Captain America", 100);
+        Hero heroThree = new Hero("Vision", 3);
+        Hero heroFour = new Hero("Iron Man", 48);
+        Hero heroFive = new Hero("Scarlet Witch", 29);
+        Hero heroSix = new Hero("Thor", 1500);
+        Hero heroSeven = new Hero("Spider-Man", 18);
+        Hero heroEight = new Hero("Hulk", 49);
+        Hero heroNine = new Hero("Doctor Strange", 42);
+
         ArrayList<Hero> heroes = new ArrayList<>();
-
-        // TODO 2 : Add those heroes to the list
-        // name: Black Widow, age: 34
-        // name: Captain America, age: 100
-        // name: Vision, age: 3
-        // name: Iron Man, age: 48
-        // name: Scarlet Witch, age: 29
-        // name: Thor, age: 1500
-        // name: Spider-Man, age: 18
-        // name: Hulk, age: 49
-        // name: Doctor Strange, age: 42
-
-        // TODO 3 : It's Thor birthday, now he's 1501
+        heroes.add(heroOne);
+        heroes.add(heroTwo);
+        heroes.add(heroThree);
+        heroes.add(heroFour);
+        heroes.add(heroFive);
+        heroes.add(heroSix);
+        heroes.add(heroSeven);
+        heroes.add(heroEight);
+        heroes.add(heroNine);
+        
+        heroSix.setAge(1501); // It's Thor birthday, now he's 1501
 
         // TODO 4 : Shuffle the heroes list
 
