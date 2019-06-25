d5532cb547df4385d7f77f11f6e20958a1ffaaef
Clément Bouakil <bouakil.clement.pro@gmail.com>
2019-06-25 18:00:12 +0200

    appel de thor

--- Thanos.java
+++ Thanos.java
@@ -5,18 +5,19 @@ public class Thanos {
     public static void main(String[] args) {
 
         ArrayList<Hero> heroes = new ArrayList<>();
-        
+
         heroes.add(new Hero("Black Widow", 34));
         heroes.add(new Hero("Captain America", 100));
         heroes.add(new Hero("Vision", 3));
         heroes.add(new Hero("Iron Man", 48));
         heroes.add(new Hero("Scarlet Witch", 29));
-        heroes.add(new Hero("Thor", 1500));
+        heroes.add(new Hero("Thor", 1500)); // indice 5
         heroes.add(new Hero("Spider-Man", 18));
         heroes.add(new Hero("Hulk", 49));
         heroes.add(new Hero("Doctor Strange", 42));
-        
-        heroSix.setAge(1501);   // It's Thor birthday, now he's 1501
+
+        Hero thor = heroes.get(5);
+        thor.setAge(1501);   // It's Thor birthday, now he's 1501
         Collections.shuffle(heroes);    // Shuffle the heroes list
 
         int length = heroes.size();
