602f16a6cb19a8c116d04e70eb32543db22c4bf8
Clément Bouakil <bouakil.clement.pro@gmail.com>
2019-06-25 13:54:23 +0200

    WIP (subList + boucle- )

--- Thanos.java
+++ Thanos.java
@@ -4,22 +4,36 @@ public class Thanos {
 
     public static void main(String[] args) {
 
+        Hero heroOne = new Hero("Black Widow", 34);
+        Hero heroTwo = new Hero("Captain America", 100);
+        Hero heroThree = new Hero("Vision", 3);
+        Hero heroFour = new Hero("Iron Man", 48);
+        Hero heroFive = new Hero("Scarlet Witch", 29);
+        Hero heroSix = new Hero("Thor", 1500);
+        Hero heroSeven = new Hero("Spider-Man", 18);
+        Hero heroEight = new Hero("Hulk", 49);
+        Hero heroNine = new Hero("Doctor Strange", 42);
+
         ArrayList<Hero> heroes = new ArrayList<>();
-        heroes.add(new Hero("Black Widow", 34));
-        heroes.add(new Hero("Black Widow", 34));
-        heroes.add(new Hero("Vision", 3));
-        heroes.add(new Hero("Iron Man", 48));
-        heroes.add(new Hero("Scarlet Witch", 29));
-        heroes.add(new Hero("Thor", 1500));
-        heroes.add(new Hero("Spider-Man", 18));
-        heroes.add(new Hero("Hulk", 49));
-        heroes.add(new Hero("Doctor Strange", 42));
+        heroes.add(heroOne);
+        heroes.add(heroTwo);
+        heroes.add(heroThree);
+        heroes.add(heroFour);
+        heroes.add(heroFive);
+        heroes.add(heroSix);
+        heroes.add(heroSeven);
+        heroes.add(heroEight);
+        heroes.add(heroNine);
         
         heroSix.setAge(1501);   // It's Thor birthday, now he's 1501
         Collections.shuffle(heroes);    // Shuffle the heroes list
 
-        // TODO 5 : Keep only the half of the list
+        int length = heroes.size();
+        ArrayList<Hero> alive = heroes.subList(0, (length+1));
 
-        // TODO 6 : Loop throught the list and display the name of the remaining heroes
+        for (int i = 0; i < alive.size(); i++) {
+            Hero stuff = lunch.get(i);
+            System.out.println(stuff.getName());
+        }
     }
 }
